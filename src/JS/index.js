a3b352d38e961b6c0fc309f7ceb273013c247bf5
Andrew Lee <acelee500@gmail.com>
2023-03-06 16:11:07 -0500

    moving to yelp api functionality

--- src/JS/index.js
+++ src/JS/index.js
@@ -3,40 +3,18 @@ let lat;
 let lng;
 let map;
 
+let place = {
+  placeAddress: '',
+  placeName: '',
+  placeStatus: '',
+  placePhoto: [],
+}
+
+let carousel = document.querySelector(".carousel");
+
+let activityList = document.getElementById('names'); 
+
 const x = document.getElementById("demo");
-
-document.querySelectorAll(".carousel").forEach((carousel) => {
-  const items = carousel.querySelectorAll(".carousel_item");
-  const buttonsHTML = Array.from(items, () => {
-    return `<span class="carousel_button"></span>`;
-  });
-  // console.log(buttonsHTML)
-  carousel.insertAdjacentHTML(
-    "beforeend",
-    `
-  <div class="carousel_nav">
-    ${buttonsHTML.join("")}
-  </div>`
-  );
-
-  const buttons = carousel.querySelectorAll(".carousel_button");
-  buttons.forEach((button, i) => {
-    button.addEventListener("click", () => {
-      //unselect all items
-      items.forEach((item) => item.classList.remove("carousel_item_selected"));
-      buttons.forEach((button) =>
-        button.classList.remove("carousel_button_selected")
-      );
-
-      items[i].classList.add("carousel_item_selected");
-      button.classList.add("carousel_button_selected");
-    });
-  });
-
-  //selects 1st item on page load
-  items[0].classList.add("carousel_item_selected");
-  buttons[0].classList.add("carousel_button_selected");
-});
 
 //  navigator.geolocation.getCurrentPosition(initMap);
 
@@ -58,10 +36,19 @@ function initMap(position) {
 
   const infowindow = new google.maps.InfoWindow();
   
+  //for search by query
   let request = {
-    query: 'fast food',
-    fields: ['name', 'geometry', 'business_status',  'formatted_address','photos'],
+    query: 'eiffel tower',
+    fields: ['name', 'geometry', 'business_status',  'formatted_address','photos', 'rating', 'icon_background_color', 'html_attributions'],
   }; 
+
+  //for search by nearby search
+  // let request = {
+  //   location: manhattan,
+  //   radius: '500',
+  //   type: ['tourist_attraction'],
+  // }; 
+
 
   const service = new google.maps.places.PlacesService(map);
 
@@ -72,20 +59,123 @@ function initMap(position) {
           position: results[i].geometry.location,
           map,
         });
-
+        console.log(results[i].html_attributions);
         marker.addListener("click", () => {
-          infowindow.setContent(results[i].name || "place");
+          infowindow.setContent(`<strong>${results[i].name}</strong> <br> ${results[i].formatted_address}`|| "place");
           infowindow.open({
             anchor: marker,
             map,
           });
         });
+        
         console.log(results[i]);
+
+        place = {
+          placeAddress: results[i].formatted_address,
+          placeName: results[i].name,
+          placeStatus: results[i].business_status,
+          placePhoto: 
+            [results[i].photos[0].getUrl({maxWidth: 1000, maxHeight: 1000})]
+            // [results[i].photos.forEach(photo => {
+            //   photo.getUrl({maxWidth: 100, maxHeight: 100});
+            // })]
+        };
+        
+        let carouselDiv = document.createElement("div");
+        carouselDiv.setAttribute("class", "carousel_item");
+
+        let image = document.createElement("img");
+        image.setAttribute("alt", place.placeName);
+        image.setAttribute("src", place.placePhoto); 
+
+
+        carouselDiv.appendChild(image);
+        carousel.appendChild(carouselDiv);
+
+        let activityDiv = document.createElement("div");
+        let activity = document.createElement("li");
+        activity.innerHTML = place.placeName;
+
+        activityDiv.appendChild(activity);
+        activityList.appendChild(activityDiv);
       }
       map.setCenter(results[0].geometry.location);
+
+      // console.log(place); 
     }
+    
+    
+    const items = carousel.querySelectorAll(".carousel_item");
+        const buttonsHTML = Array.from(items, () => {
+          return `<span class="carousel_button"></span>`;
+        });
+      
+        carousel.insertAdjacentHTML(
+          "beforeend",
+          `
+        <div class="carousel_nav">
+          ${buttonsHTML.join("")}
+        </div>`
+        );
+      
+        const buttons = carousel.querySelectorAll(".carousel_button");
+        buttons.forEach((button, i) => {
+          button.addEventListener("click", () => {
+            //unselect all items
+            items.forEach((item) => item.classList.remove("carousel_item_selected"));
+            buttons.forEach((button) =>
+              button.classList.remove("carousel_button_selected")
+            );
+      
+            items[i].classList.add("carousel_item_selected");
+            button.classList.add("carousel_button_selected");
+          });
+        });
+      
+        if (items.length > 0){
+          //selects 1st item on page load
+          items[0].classList.add("carousel_item_selected");
+          buttons[0].classList.add("carousel_button_selected");
+        }
   })
 
+  
+  // let request1 = {
+  //   query: 'washington square park',
+  //   fields: ['name', 'geometry', 'business_status',  'formatted_address','photos'],
+  // }; 
+
+  // service.findPlaceFromQuery(request1, function(results, status) {
+  //   if (status === google.maps.places.PlacesServiceStatus.OK){
+  //     for (let i = 0; i < results.length; i++){
+  //       const marker = new google.maps.Marker({
+  //         position: results[i].geometry.location,
+  //         map,
+  //         // icon: results[i].photos[0].getUrl({maxWidth: 100, maxHeight: 100}),
+  //       });
+
+  //       marker.addListener("click", () => {
+  //         infowindow.setContent( results[i].name|| "place");
+  //         infowindow.open({
+  //           anchor: marker,
+  //           map,
+  //         });
+  //       });
+  //       // console.log(results[i]);
+  //     }
+      
+      
+  //     place = {
+  //       placeAddress: results[0].formatted_address,
+  //       placeName: results[0].name,
+  //       placeStatus: results[0].business_status
+  //     };
+      
+
+  //     console.log(place); 
+      
+  //   }
+  // })
 
   // const marker = new google.maps.Marker({
   //   position: manhattan,
@@ -101,6 +191,8 @@ function initMap(position) {
   // });
 }
 
+
+//////////////////////////////// //////////////////////////////////
 
 /*
 Method to get current location
