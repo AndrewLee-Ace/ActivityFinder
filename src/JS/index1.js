7234be61f07caddd02f262ea260031ba48df8249
Andrew Lee <acelee500@gmail.com>
2023-05-08 09:37:15 -0400

    Added Excel Data Log

--- src/JS/index1.js
+++ src/JS/index1.js
@@ -1,3 +1,5 @@
+import express from 'express';
+
 let map;
 
 let place = {
@@ -12,6 +14,7 @@ let carousel = document.querySelector(".carousel");
 let activityList = document.getElementById("names");
 
 let suggestion = document.getElementById("suggestion");
+
 
 // const x = document.getElementById("demo");
 
